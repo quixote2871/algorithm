aa2c9580e8eefde02c854753d2a990e1b1a71836
quixote2871 <quixote2871@gmail.com>
2025-03-26 14:17:46 +0900

    feat: euler totient function

---
+++ math.cpp
@@ -0,0 +1,13 @@
+long long euler_phi(long long n) {
+	long long res = n;
+	for(long long i=2; i*i<=n; i++) {
+		if(n%i==0) {
+			while(n%i==0)
+				n /= i;
+			res = res/i*(i-1);
+		}
+	}
+	if(n!=1)
+		res = res/n*(n-1);
+	return res;
+}
